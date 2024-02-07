7ff1906ae6152bb2fe304ffc26d215e04f5ac9e0
n-jokic <nikitajokic98@gmail.com>
2024-02-07 19:54:50 +0100

    function for full analysis of linearized model
    stability, observability and controllability. Returns linearized model in state space, in symbolic form as transfer function and bool flags for stability, contrl. and obs.

---
+++ src/linear_dynamics_analysis.m
@@ -0,0 +1,57 @@
+function [A, B, C, D, G, controllable, observable, stable] = ...
+    linear_dynamics_analysis(system_dyn, ...
+    x_linearization_point, ...
+    u_linearization_point, ....
+        observation_matrix, print_info)
+
+if nargin == 4
+    print_info = true;
+end
+
+true_false = ["false"; "true"];
+
+[A, B, C, D] = linearize(system_dyn, x_linearization_point, ...
+    u_linearization_point, observation_matrix);
+
+if print_info
+    disp('--------');
+    disp(['linearization around x = ' ...
+        num2str(round(x_linearization_point', 3)) ...
+        ', u = ' num2str(round(u_linearization_point, 3))]);
+    disp('A: ');
+    disp(A);
+    disp('B: ');
+    disp(B);
+    disp('C: ');
+    disp(C);
+    disp('D: ');
+    disp(D);
+    disp('--------');
+end
+[stable, G, system_eigenvalues] = is_stable(A, B, C, D);
+
+if print_info
+    disp('System is stable: ');
+    disp(true_false(stable+1));
+    disp('eigenvalues: ');
+    disp(system_eigenvalues);
+    disp('--------');
+end
+
+controllable = is_controllable(A, B);
+if print_info
+    disp('System is controlable: ');
+    disp(true_false(controllable+1));
+    disp('--------');
+end
+
+observable = is_observable(A, C);
+
+if print_info
+    disp('System is observable: ');
+    disp(true_false(observable+1));
+    disp('--------');
+end
+
+end
+
