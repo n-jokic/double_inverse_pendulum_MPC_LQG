ff8702472d90d0d7cfa663f9cf6a9919808fdb89
n-jokic <nikitajokic98@gmail.com>
2024-02-10 16:01:59 +0100

    Update inverted_pendulum_measurement.m

--- src/inverted_pendulum_measurement.m
+++ src/inverted_pendulum_measurement.m
@@ -1,6 +1,7 @@
 function y = inverted_pendulum_measurement(state_vector)
 
 % x(1) - Horizontal arm angle x(3) - pendulum angle
-y = [state_vector(1); state_vector(3)];
+y = [state_vector(1); state_vector(2); state_vector(3); state_vector(4);
+    state_vector(5)];
 end
 
