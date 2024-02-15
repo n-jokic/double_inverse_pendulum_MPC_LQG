21691072a8a6d1225842f208a428f4a3eb3a9989
n-jokic <nikitajokic98@gmail.com>
2024-02-15 19:50:08 +0100

    added plotting of state and control reference

--- src/simulate_system.m
+++ src/simulate_system.m
@@ -1,19 +1,27 @@
-function [x, u] = simulate_system(system_dynamics, controller, x0, u0, ...
-    state_reference, control_reference, disturbance, sigma_noise,t, M)
-u = zeros(length(t), length(u0));
-x = zeros(length(t)+1, length(x0));
+function [states, control, states_measured] = simulate_system( ...
+    system_dynamics, controller, x0, u0, ...
+    state_reference, control_reference, disturbance, ...
+    sigma_noise, t, M, u_limit)
+control = zeros(length(t), length(u0));
+states = zeros(length(t)+1, length(x0));
+states_measured = zeros(length(t)+1, length(x0));
 dt = t(2) - t(1);
 
-x(2, :) = rk4_integration(x0, u0, system_dynamics, dt, M);
+states(2, :) = rk4_integration(x0, u0, system_dynamics, dt, M);
 
-u(1, :) = u0;
-x(1, :) = x0;
+control(1, :) = u0;
+states(1, :) = x0;
+states_measured(1, :) = x0;
 
 for i = 2 : length(t)
     noise_sample = mvnrnd([0; 0; 0; 0; 0], sigma_noise)';
-    u(i, :) = controller(x(i, :)'+noise_sample, state_reference(i, :)') ...
+    states_measured(i, :) = states(i, :)'+noise_sample;
+    control(i, :) = controller(states_measured(i, :)', state_reference(i, :)') ...
     + disturbance(i, :) + control_reference(i, :);
-    x(i+1, :) = rk4_integration(x(i, :)', u(i, :), ...
+
+    control(i, :) = max(min(control(i, :), u_limit(2)), u_limit(1));
+
+    states(i+1, :) = rk4_integration(states(i, :)', control(i, :), ...
                                 system_dynamics, dt, M);
 end
 
