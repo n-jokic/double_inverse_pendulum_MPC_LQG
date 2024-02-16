1dc3b6089dfe0c88e1277a58bf35d7474e700cd6
n-jokic <nikitajokic98@gmail.com>
2024-02-16 23:28:21 +0100

    Update energy_shaping.m

--- src/ad-hoc swingup/energy_shaping.m
+++ src/ad-hoc swingup/energy_shaping.m
@@ -1,10 +1,13 @@
 function u = energy_shaping(x, r)
     % u =  k1 (x2 + k2 cos(x3)x4)
+
     
-    k1 = 0.5; % k1 > 0 0.5
+    k1 = 1; % k1 > 0 0.5
     k2 = 0.9; % k2 > 0  0.9
     
-    u = k1*x(2) + k2*cos(x(3))*x(4);
-
+    u = k1*(x(2) + k2*cos(x(3))*x(4));
+    
+  
+    
     
 end
